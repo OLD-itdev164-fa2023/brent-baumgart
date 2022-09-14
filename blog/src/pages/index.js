1031993326ba7a12f02209596992242ad03526d2
Brent <Brent@DESKTOP-84TJ9G9>
2022-09-13 20:12:15 -0500

    updated pages/index.js for images and excerpts

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,20 +1,30 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from  "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
-const IndexPage = ({data})=>(
+const IndexPage = ({ data }) => (
+  
   <Layout>
     <Seo title="Home"/>
-    <ul>
+    <ul className={styles.list}>
       {
-        data.allContentfulBlogPost.edges.map(edge =>(
-
-          <li>
-          <Link to={edge.node.slug} key={edge.node.id}>{edge.node.title}
-          </Link>
+        data.allContentfulBlogPost.edges.map(edge => (
+          <li key={edge.node.id}>
+            <Link to={edge.node.slug}>{edge.node.title}</Link>
+          <div>
+            <GatsbyImage 
+              image={edge.node.heroImage.gatsbyImageData}
+            />
+          </div>
+          <div>
+            {edge.node.body.childMarkdownRemark.excerpt}
+          </div>
           </li>
         ))
+
       }
     </ul>
   </Layout>
@@ -23,15 +33,27 @@ const IndexPage = ({data})=>(
 export default IndexPage
 
 export const query = graphql`
-  {
-    allContentfulBlogPost {
-      edges {
-        node {
-          id
-          slug
-          title
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        title
+        slug
+        body {
+          childMarkdownRemark {
+            excerpt 
+          }
+        }
+        heroImage {
+          gatsbyImageData (
+            layout:CONSTRAINED
+            placeholder:BLURRED
+            width: 300
+          )
         }
       }
     }
   }
+}
 `
